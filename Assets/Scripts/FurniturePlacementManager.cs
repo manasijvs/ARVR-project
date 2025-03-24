bb7838e7e2f034e70aabc0d5b6b8fbfea4747f38
Manasij VS <manasijvs972003@gmail.com>
2025-03-25 00:13:38 +0530

    updated scripts

---
+++ Assets/Scripts/FurniturePlacementManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using Unity.XR.CoreUtils;
+public class FurniturePlacementManager : MonoBehaviour
+{
+    public GameObject SpawnableFurniture;
+    public XROrigin xrOrigin;
+    public ARRaycastManager raycastManager;
+    public ARPlaneManager planeManager;
+    private List<ARRaycastHit>raycastHits = new List<ARRaycastHit>();
+
+    private void Update()
+    {
+        if(Input.touchCount>0)
+        {
+            if(Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                bool collision = raycastManager.Raycast(Input.GetTouch(0).position,raycastHits, TrackableType.PlaneWithinPolygon);
+
+                if(collision)
+                {
+                    GameObject _object = Instantiate(SpawnableFurniture);
+                    _object.transform.position = raycastHits[0].pose.position;
+                    _object.transform.rotation = raycastHits[0].pose.rotation;
+                }
+                foreach(var planes in planeManager.trackables)
+                {
+                    planes.gameObject.SetActive(false);
+                }
+                planeManager.enabled = false;
+            }
+        }
+    }
+}
